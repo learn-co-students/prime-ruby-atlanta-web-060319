7de0759d2dc6e3d839c0b4bee7bf10dd321ecbab
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-30 15:06:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 # Add  code here!
 def prime?(x)
-  if x% 2 ==0 && x!=2
-     false
-  else
-    true
-  end
+  # if x% 2 ==0 && x!=2
+  #    false
+  # else
+  #   true
+  # end
+  (2...integer).none? {|i| integer % i == 0}
 end
