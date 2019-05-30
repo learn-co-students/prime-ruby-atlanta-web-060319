9c6b95c1a97a12094d59437537a0f004dec82e80
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-30 15:28:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,4 @@
-# Add  code here!
 def prime?(x)
-  if x<0 || x == 0 || x == 1
-     false
-  elsif x%2!=0 || x==2
-    true
-  else
-    false
-  end
+  return false if (x.even? && x>2) || x<= 1
+
 end
