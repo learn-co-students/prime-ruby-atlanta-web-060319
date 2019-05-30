4c1dee68def41246adf4eb509976ce23090308c6
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-30 15:33:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(x)
-  return false if (x.even? && x>2) || x<= 1
-  y=2
-  while y<x
-    return false if x %y ==0
-    x+=1
+  return false if ( x.even? && x >2 ) || x <= 1
+
+  y = 2
+
+  while y < x
+    return false if x % y == 0
+    x += 1
   end
   true
 end
